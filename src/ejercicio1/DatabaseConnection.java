98fa45b475eaf801b474365c582579255650f910
Fabio Bulnes <bsiesquenfabioa@uss.edu.pe>
2026-04-21 10:30:27 -0500

    Update DatabaseConnection.java

--- src/ejercicio1/DatabaseConnection.java
+++ src/ejercicio1/DatabaseConnection.java
@@ -5,13 +5,12 @@ public class DatabaseConnection {
     private static volatile DatabaseConnection instance;
     private String url;
 
-    // Constructor privado
+    
     private DatabaseConnection() {
         this.url = "jdbc:mysql://localhost:3306/miBD";
         System.out.println("Conexión creada a: " + url);
     }
-
-    // Singleton con doble verificación
+   
     public static DatabaseConnection getInstance() {
         if (instance == null) {
             synchronized (DatabaseConnection.class) {
@@ -23,7 +22,6 @@ public class DatabaseConnection {
         return instance;
     }
 
-    // Simulación de consulta
     public void ejecutarConsulta(String sql) {
         System.out.println("Ejecutando: " + sql +
                 " | Instancia ID: " + this.hashCode());
